b46810bc5b824dac3793036b1a164ebd3a2d51d5
James Coulter <jimcoulter85@gmail.com>
2019-10-22 08:45:16 -0700

    rez adjustment

--- ULV_Tank.py
+++ ULV_Tank.py
@@ -17,7 +17,7 @@ import sys
 from pygame.locals import *
 
 IP_ADDRESS = "10.0.0.125"  # ip address of host used for video stream
-RASPICAM_ON = "raspivid -t 999999 -w 1920 -h 1080 -sa -50 -br 60 -co 20 -fps 24 -b 3000000 -o - | gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=" + IP_ADDRESS + " port=8160&"
+RASPICAM_ON = "raspivid -t 999999 -w 1280 -h 720 -sa -50 -br 60 -co 20 -fps 24 -b 3000000 -o - | gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=" + IP_ADDRESS + " port=8160&"
 RASPICAM_OFF = "sudo killall -9 gst-launch-1.0"
 PHOTOS_DIR = "/home/pi/Desktop/Tank_photos"  # save photos to a any directory
 SNAP_PHOTO = "raspistill -w 1920 -h 1080 -t 500 -o " + PHOTOS_DIR + "/"
